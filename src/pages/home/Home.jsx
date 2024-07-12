a9fbbf38078b625608c56565460debc77d0d4612
Nitzan Bukchin <121240414+NitBuk@users.noreply.github.com>
2024-07-12 16:29:37 +0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+// Home.js
+import React, { useState, useContext } from 'react';
 import Topbar from "../../components/topbar/Topbar";
 import Feed from '../../components/HomeComponents/feed/Feed';
 import Rightbar from "../../components/rightbar/Rightbar";
 import styled from 'styled-components';
-// TODO : customise the page for the user
-// import { getUserData } from '../../firestore/users';
-// import { auth } from '../../firebase-config';
+import { AuthContext } from '../../context/AuthContext'; // Adjust the path as needed
 
 const HomeContainer = styled.div`
   display: flex;
@@ -14,6 +13,7 @@ const HomeContainer = styled.div`
 `;
 
 function Home() {
+  const { user, login, logout } = useContext(AuthContext);
   const [showFavorites, setShowFavorites] = useState(false);
 
   const openFavorites = () => {
@@ -35,6 +35,16 @@ function Home() {
         />
         <Rightbar openFavorites={openFavorites} />
       </HomeContainer>
+      // test begin
+      {user ? (
+        <div>
+          <p>Welcome, {user.displayName}!</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={() => login({ name: "John Doe" })}>Login</button>
+      )}
+      // test end
     </>
   );
 }
