c7732e61ebee905e4d61d66c68e499f2870df52a
Nitzan Bukchin <121240414+NitBuk@users.noreply.github.com>
2024-07-12 17:30:12 +0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 // Home.jsx
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Topbar from "../../components/topbar/Topbar";
 import Feed from '../../components/HomeComponents/feed/Feed';
 import Rightbar from "../../components/rightbar/Rightbar";
@@ -16,7 +15,6 @@ const HomeContainer = styled.div`
 function Home() {
   const { user, userData, logout } = useContext(AuthContext);
   const [showFavorites, setShowFavorites] = useState(false);
-  const navigate = useNavigate(); // Hook from react-router-dom
 
   const openFavorites = () => {
     setShowFavorites(true);
@@ -26,13 +24,17 @@ function Home() {
     setShowFavorites(false);
   };
 
-  const handleLoginRedirect = () => {
-    navigate('/Login'); // Redirect to Login page
-  };
-
   return (
     <>
       <Topbar />
+      <UserInfo>
+        {currentUser && (
+          <>
+            <UserImage src={currentUser.photoURL} alt={`${currentUser.displayName}'s profile`} />
+            <span>{currentUser.displayName}</span>
+          </>
+        )}
+      </UserInfo>
       <HomeContainer>
         <Feed 
           showFavorites={showFavorites} 
@@ -41,20 +43,6 @@ function Home() {
         />
         <Rightbar openFavorites={openFavorites} />
       </HomeContainer>
-      {user ? (
-        <div>
-          <p>Welcome, {user.displayName}!</p>
-          <button onClick={logout}>Logout</button>
-        </div>
-      ) : (
-        <button onClick={handleLoginRedirect}>Login</button>
-      )}
-      {userData && (
-        <div>
-          <p>Bio: {userData.bio}</p>
-          <p>User ID: {userData.id}</p>
-        </div>
-      )}
     </>
   );
 }
