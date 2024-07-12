b066a0cc74f523bc1cefae80110df2a5fe83dfe7
Nitzan Bukchin <121240414+NitBuk@users.noreply.github.com>
2024-07-12 16:41:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // App.js
-
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // Adjust the path as needed
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './firestore/AuthContext'; // Adjust the path as needed
 
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -15,18 +14,20 @@ import OnBoarding1 from './pages/on_boarding/OnBoarding';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="/OnBoarding" element={<OnBoarding1 />} />
-      <Route path="/Profile" element={<Profile />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Register" element={<Register />} />
-      <Route path="/Inbox" element={<Inbox />} />
-      <Route path="/MusicPlayer" element={<MusicPlayer />} />
-      <Route path="/PDFReader" element={<PDFReader />} />
-    </Routes>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/OnBoarding" element={<OnBoarding1 />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Inbox" element={<Inbox />} />
+          <Route path="/MusicPlayer" element={<MusicPlayer />} />
+          <Route path="/PDFReader" element={<PDFReader />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
-
-// export default App;
